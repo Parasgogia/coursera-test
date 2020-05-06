ced8a54e5151b5fcb6188b3a81fed4d67a15e7cf
Paras Gogia <parasgogia1@gmail.com>
2020-05-06 18:34:51 +0530

    Assignment 1

--- app.js
+++ app.js
@@ -171,7 +171,7 @@
 
   })();
 
-  */
+  
 
   (function(){
     'user strict';
@@ -224,3 +224,49 @@
     
     
   })();
+
+  */
+
+ (function(){
+  'user strict';
+
+  angular.module('LunchCheck' , [])
+  
+
+  .controller('LunchBox' , LunchCheckController);
+  LunchCheckController.inject=['$scope'];
+  
+  function LunchCheckController($scope){
+    $scope.name='';
+    $scope.message='';
+    $scope.LuchCheckBox= function(){
+      
+
+      var str= $scope.name;
+      var string = str.split('');
+      console.log(string.length);
+      console.log(string);
+      if(string.length>3){
+        
+        $scope.message='Too much!';
+      }
+      if(string.length<=3 & string.length>0){
+        $scope.message='Enjoy!';
+      }
+      if(string.length==0 | str ==','){
+        $scope.message='Please enter data first';
+        
+      }
+      else{
+        return '';
+      }
+
+
+    };
+
+  };
+    
+
+
+
+})();
